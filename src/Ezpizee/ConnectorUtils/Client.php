f053a4d8c7a251cf96bcc17edb5f7c1a0b058c4a
Sothea Nim <sothea.nim@ezpizee.com>
2020-12-25 20:14:32 -0500

    improved

--- src/Ezpizee/ConnectorUtils/Client.php
+++ src/Ezpizee/ConnectorUtils/Client.php
@@ -8,155 +8,21 @@ use Unirest\Request;
 
 class Client extends MicroserviceClient
 {
-  const HEADER_PARAM_ACCEPT = "Accept";
-  const HEADER_PARAM_CTYPE = "Content-Type";
-  const HEADER_PARAM_ACCESS_TOKEN = "Authorization";
-  const HEADER_PARAM_USER_AGENT = "User-Agent";
-  const HEADER_VALUE_JSON = "application/json";
-  const HEADER_VALUE_USER_AGENT = "Ezpizee Web/1.0";
-  const HEADER_PARAM_APP_NAME = "App-Name";
-  const HEADER_PARAM_APP_VERSION = "App-Version";
-  const HEADER_VALUE_APP_VERSION = "0.0.1";
-  const HEADER_PARAM_APP_PLATFORM = "App-Platform";
-  const HEADER_VALUE_APP_PLATFORM = "Unknown";
-  const HEADER_PARAM_OS_PLATFORM_VERSION = "OS-Platform-Version";
-  const HEADER_VALUE_OS_PLATFORM_VERSION = "Unknown";
-  const HEADER_LANGUAGE_TAG = "Language-Tag";
-
-  private $isMultipart = false;
-  private $platform = '';
-  private $platformVersion = '';
-  
-  public function __construct(string $schema, string $host, Config $config)
-  {
-    parent::__construct($schema, $host, $config);
-    $this->setDefaultHeaders();
-  }
-
-  public function setMultipart(bool $b): void {$this->isMultipart = $b;}
-
-  public function verifyPeer(bool $b): void {Request::verifyPeer($b);}
-
-  public function setPlatform(string $platform): void {$this->platform=$platform;}
-
-  public function setPlatformVersion(string $platformVersion): void {$this->platformVersion=$platformVersion;}
-
-  private function setDefaultHeaders(): void {
-    if (!$this->isMultipart && !$this->hasHeader(self::HEADER_PARAM_CTYPE)) {
-      $this->addHeader(self::HEADER_PARAM_CTYPE, self::HEADER_VALUE_JSON);
-    }
-    if (!$this->hasHeader(self::HEADER_PARAM_ACCEPT)) {
-      $this->addHeader(self::HEADER_PARAM_ACCEPT, self::HEADER_VALUE_JSON);
-    }
-    if (!$this->hasHeader(self::HEADER_PARAM_USER_AGENT)) {
-      $this->addHeader(self::HEADER_PARAM_USER_AGENT, self::HEADER_VALUE_USER_AGENT);
-    }
-    if (!$this->hasHeader(self::HEADER_PARAM_APP_VERSION)) {
-      $this->addHeader(self::HEADER_PARAM_APP_VERSION, self::HEADER_VALUE_APP_VERSION);
-    }
-    if (!$this->hasHeader(self::HEADER_PARAM_APP_PLATFORM)) {
-      $this->addHeader(self::HEADER_PARAM_APP_PLATFORM, $this->platform?$this->platform:self::HEADER_VALUE_APP_PLATFORM);
-    }
-    if (!$this->hasHeader(self::HEADER_PARAM_OS_PLATFORM_VERSION)) {
-      $this->addHeader(self::HEADER_PARAM_OS_PLATFORM_VERSION, $this->platformVersion?$this->platformVersion:self::HEADER_VALUE_OS_PLATFORM_VERSION);
-    }
-    if (!$this->hasHeader(self::HEADER_PARAM_APP_NAME)) {
-      $this->addHeader(self::HEADER_PARAM_APP_NAME, $this->getConfig('app_name'));
-    }
-    $this->bearerToken();
-  }
-
-  private function bearerToken(): void {
-    if (!$this->hasHeader(self::HEADER_PARAM_ACCESS_TOKEN)) {
-      if (!isset($_COOKIE['ezpz_token'])) {
-        $tokenUri = $this->getConfig('token_uri');
-        if ($this->getConfig('env') === 'local') {
-          $this->verifyPeer(false);
-        }
-        $response = Request::post(
-          $this->url($tokenUri), $this->getHeaders(),
-          null,
-          $this->getConfig('client_id'),
-          $this->getConfig('client_secret')
-        );
-        if (
-          isset($response->body->data)
-          && isset($response->body->data->AuthorizationBearerToken)
-          && isset($response->body->data->expire_in)
-        ) {
-          setcookie('ezpz_token', $response->body->data->AuthorizationBearerToken, time() + ($response->body->data->expire_in - (10 * 60 * 1000)), "/");
-          $this->addHeader(self::HEADER_PARAM_ACCESS_TOKEN, 'Bearer ' . $response->body->data->AuthorizationBearerToken);
-        }
-      }
-      else {
-        $this->addHeader(self::HEADER_PARAM_ACCESS_TOKEN, 'Bearer ' . $_COOKIE['ezpz_token']);
-      }
-    }
-  }
-
   public static function getTokenUri(): string {return '/api/user/token';}
 
   public static function adminUri(string $platform='ezpz', string $version='latest'): string {return '/adminui/'.$version.'/index.'.$platform.'.html';}
 
   public static function installUri(string $platform='ezpz'): string {return '/install/html/index.'.$platform.'.html';}
 
-  public static function apiSchema(string $env): string {
-    switch ($env) {
-      case 'local':
-        return 'https://';
-      case 'dev':
-        return 'https://';
-      case 'stage':
-        return 'https://';
-      case 'prod':
-      default:
-        return 'https://';
-    }
-  }
+  public static function apiSchema(string $env): string {return 'https://';}
 
-  public static function apiHost(string $env): string {
-    switch ($env) {
-      case 'local':
-        return $env.'-api.ezpizee.com';
-      case 'dev':
-        return $env.'-api.ezpz.solutions';
-      case 'stage':
-        return $env.'-api.ezpizee.com';
-      case 'prod':
-      default:
-        return 'api.ezpizee.com';
-    }
-  }
+  public static function apiHost(string $env): string {return $env==='dev'?$env.'-api.ezpz.solutions':($env==='prod'?'':$env.'-').'api.ezpizee.com';}
 
   public static function apiEndpointPfx(string $env): string {return self::apiSchema($env).self::apiHost($env);}
 
-  public static function cdnSchema(string $env): string {
-    switch ($env) {
-      case 'local':
-        return 'https://';
-      case 'dev':
-        return 'https://';
-      case 'stage':
-        return 'https://';
-      case 'prod':
-      default:
-        return 'https://';
-    }
-  }
+  public static function cdnSchema(string $env): string {return 'https://';}
 
-  public static function cdnHost(string $env): string {
-    switch ($env) {
-      case 'local':
-        return $env.'-cdn.ezpz.solutions';
-      case 'dev':
-        return $env.'-cdn.ezpz.solutions';
-      case 'stage':
-        return $env.'-cdn.ezpz.solutions';
-      case 'prod':
-      default:
-        return 'cdn.ezpz.solutions';
-    }
-  }
+  public static function cdnHost(string $env): string {return ($env==='prod'?'':$env.'-').'cdn.ezpz.solutions';}
 
   public static function cdnEndpointPfx(string $env): string {return self::cdnSchema($env).self::cdnHost($env);}
 }
--- src/Ezpizee/ConnectorUtils/JWT.php
+++ src/Ezpizee/ConnectorUtils/JWT.php
@@ -2,7 +2,55 @@
 
 namespace Ezpizee\ConnectorUtils;
 
+use Lcobucci\JWT\Builder;
+use Lcobucci\JWT\Parser;
+use Lcobucci\JWT\Signer\Rsa\Sha256;
+use Lcobucci\JWT\Token;
+use Lcobucci\JWT\ValidationData;
+
 class JWT
 {
+  const JWT_API_CORE_ISSUER = "ezpz.api.core";
 
+  private function __construct(){}
+
+  public static final function encode(string $audience, array $data): string {
+    $signer = new Sha256();
+    return (new Builder())
+      ->withHeader('alg', $signer->getAlgorithmId())
+      ->withHeader('typ', 'JWT')
+      ->issuedBy(self::JWT_API_CORE_ISSUER)
+      ->permittedFor($audience)
+      ->withClaim('data', json_encode($data))
+      ->getToken($signer);
+  }
+
+  public static function isValidToken(string $audience, string $token): bool {
+    if (!empty($token)) {
+      $parsedToken = (new Parser())->parse($token);
+      $validationData = new ValidationData();
+      $validationData->setIssuer(self::JWT_API_CORE_ISSUER);
+      $validationData->setAudience($audience);
+      $signer = new Sha256();
+      return $parsedToken->validate($validationData) && $parsedToken->getHeader('alg') === $signer->getAlgorithmId();
+    }
+    return false;
+  }
+
+  public static final function parseToken(string $token): Token {return (new Parser())->parse($token);}
+
+  public static final function getDataFromToken(string $token): string {
+    $parsed = self::parseToken($token);
+    return $parsed->hasClaim('data') ? $parsed->getClaim('data') : null;
+  }
+
+  public static final function getIssuerFromToken(string $token): string {
+    $parsed = self::parseToken($token);
+    return $parsed->hasClaim('iss') ? $parsed->getClaim('iss') : null;
+  }
+
+  public static final function getAudienceFromToken(string $token): string {
+    $parsed = self::parseToken($token);
+    return $parsed->hasClaim('aud') ? $parsed->getClaim('aud') : null;
+  }
 }
