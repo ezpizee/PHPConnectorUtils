60827932e5196fa51311ba9816782ea680b6060c
pangkhon <pang.khon@ezpizee.com>
2021-03-15 13:42:43 +0700

    update end point cart

--- src/Ezpizee/ConnectorUtils/Endpoints.php
+++ src/Ezpizee/ConnectorUtils/Endpoints.php
@@ -12,11 +12,16 @@ class Endpoints
     const ACTIVATE = "/api/user/activate/{id}";
     const PRODUCT_ITEM = "/api/pim/product/item/{id}";
     const PRODUCT_PRICE = "/api/pim/product/price/{id}";
+    const CART_CREATE = "/api/cart/create/{store_id}";
+    const CART_CONTENT = "/api/cart/content/by/id/{id}";
+    const CART_ADD_ITEM = "/api/cart/add/items/{cart_id}";
 
-    public static function endpoint(string $str, array $params=[]): string {
+
+    public static function endpoint(string $str, array $params = []): string
+    {
         if (!empty($params)) {
-            foreach ($params as $k=>$v) {
-                $str = str_replace('{'.$k.'}', $v, $str);
+            foreach ($params as $k => $v) {
+                $str = str_replace('{' . $k . '}', $v, $str);
             }
         }
         return $str;
