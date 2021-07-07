394ac1dae62934e16c4eae79988333571854eb8e
khon pang <pang.khon@ezpizee.com>
2021-07-07 17:08:01 +0700

    update endpoint

--- src/Ezpizee/ConnectorUtils/Endpoints.php
+++ src/Ezpizee/ConnectorUtils/Endpoints.php
@@ -18,7 +18,7 @@ class Endpoints
     const PAYMENT_VERIFY = "/api/payment/check/byorder/{cart_id}";
     const PRODUCT_ITEM = "/api/pim/product/item/{id}";
     const PRODUCT_PRICE = "/api/pim/product/price/{id}";
-    const PRODUCT_LIST = "/api/pim/product/list/bychannel/web";
+    const PRODUCT_LIST = "/api/pim/product/list/bychannel/web/numperpage/{number_per_page}/start/{start}";
     const CART_CREATE = "/api/cart/create/{store_id}";
     const CART_UPDATE = "/api/cart/update/{id}";
     const CART_EMPTY = "/api/cart/empty/{cart_id}";
